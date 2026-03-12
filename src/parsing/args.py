61002f9574911a8e82ae0875c94974358b795c6a
Laurent Bonnet <laurent.bonnet26@gmail.com>
2026-03-12 15:22:24 +0100

    update: args.py in progress

--- src/parsing/args.py
+++ src/parsing/args.py
@@ -1,7 +1,12 @@
-from argparse import ArgumentParser
+from argparse import ArgumentParser, NameSpace
 
 
-def parsed_arguments():
+def parsed_arguments() -> NameSpace:
     parser = ArgumentParser()
-    parser.add_argument()
-    args = parser.parse_args()
+    parser.add_argument("--functions_definition",
+                        help="functions_definition file path",
+                        default="data/input/functions_definition.json")
+    parser.add_argument("--input",
+                        help="input file path",
+                        default="data/input/function_calling_tests.json")
+    return parser.parse_args()
